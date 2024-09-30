bae61f95e45a7fcb44736bdfd9bd1211be0aef18
Antoni Viros i Martin <aviros@ibm.com>
2024-09-30 17:09:24 -0400

    Add digest output to script (v4)

--- nightly-pytorch-compile/buildAndPush.sh
+++ nightly-pytorch-compile/buildAndPush.sh
@@ -22,4 +22,4 @@ docker build -t "${TAG}" .
 
 docker push "${TAG}"
 
-echo "digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${TAG}) >> $GITHUB_OUTPUT
+echo "digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${TAG})" >> $GITHUB_OUTPUT
